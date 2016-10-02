9764735c5373126a09e73632eb50002ce1203f67
w1lldone <wildan.arrahman@gmail.com>
2016-10-02 09:52:21 +0700

    pisah modal dengan kepengurusan

---
+++ modal.php
@@ -0,0 +1,109 @@
+<!-- Modal unutk aji-->
+<div class="modal fade" id="aji" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                <h4 class="modal-title" id="myModalLabel">More About Aji</h4>
+            </div>
+            <div class="modal-body">
+                <center>
+                    <img src="img/ph/ajek.jpg" name="aboutme" width="140" height="140" border="0" class="img-circle"></a>
+                    <h3 class="media-heading" style="margin-top: 20px;">Nugrahanto Aji Wicaksono</h3>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <i class="fa fa-3x fa-calendar"></i>
+                            <br> 
+                            <span class="label label-warning" style="margin-top: 10px;">10 Agustus 94</span>
+                        </div>
+                        <div class="col-lg-4">
+                            <i class="fa fa-3x fa-home"></i>
+                            <br> 
+                            <span class="label label-info" style="margin-top: 10px;">Banguntapan Bantul</span>
+                        </div>
+                        <div class="col-lg-4">
+                            <i class="fa fa-3x fa-briefcase"></i>
+                            <br> 
+                            <span class="label label-success" style="margin-top: 10px;">TIP 13</span>
+                        </div>
+                    </div>
+                </center>
+                <hr>
+                <center>
+                    <p class="text-left"><strong>Bio: </strong><br>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem dui, tempor sit amet commodo a, vulputate vel tellus.</p>
+                        <br>
+                    </center>
+                </div>
+                <div class="modal-footer text-center">
+                        <div class="col-lg-4">
+                            <a href="#"><i class="fa fa-facebook fa-2x"></i></a>
+                        </div>
+                        <div class="col-lg-4">
+                            <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+                        </div>
+                        <div class="col-lg-4">
+                            <a href="#"><i class="fa fa-instagram fa-2x"></i></a>
+                        </div>
+                </div>
+            </div>
+            <!--modal body -->
+        </div>
+        <!--modal content -->
+    </div>
+    <!-- modal -->
+
+<!-- Modal unutk jadug-->
+<div class="modal fade" id="jadug" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                <h4 class="modal-title" id="myModalLabel">More About jadug</h4>
+            </div>
+            <div class="modal-body">
+                <center>
+                    <img src="img/ph/jadug.jpg" name="aboutme" width="140" height="140" border="0" class="img-circle"></a>
+                    <h3 class="media-heading" style="margin-top: 20px;">Hargiono Anityo P</h3>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <i class="fa fa-3x fa-calendar"></i>
+                            <br> 
+                            <span class="label label-warning" style="margin-top: 10px;">10 Juni 95</span>
+                        </div>
+                        <div class="col-lg-4">
+                            <i class="fa fa-3x fa-home"></i>
+                            <br> 
+                            <span class="label label-info" style="margin-top: 10px;">Banyumanik Semarang</span>
+                        </div>
+                        <div class="col-lg-4">
+                            <i class="fa fa-3x fa-briefcase"></i>
+                            <br> 
+                            <span class="label label-success" style="margin-top: 10px;">TIP 13</span>
+                        </div>
+                    </div>
+                </center>
+                <hr>
+                <center>
+                    <p class="text-left"><strong>Bio: </strong><br>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sem dui, tempor sit amet commodo a, vulputate vel tellus.</p>
+                        <br>
+                    </center>
+                </div>
+                <div class="modal-footer text-center">
+                        <div class="col-lg-4">
+                            <a href="#"><i class="fa fa-facebook fa-2x"></i></a>
+                        </div>
+                        <div class="col-lg-4">
+                            <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+                        </div>
+                        <div class="col-lg-4">
+                            <a href="#"><i class="fa fa-instagram fa-2x"></i></a>
+                        </div>
+                </div>
+            </div>
+            <!--modal body -->
+        </div>
+        <!--modal content -->
+    </div>
+    <!-- modal -->
